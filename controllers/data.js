56fec64f7c8200b47b300652498a868b6b941409
DimaKreed <dimonvarch2001@gmail.com>
2021-03-01 08:45:39 +0200

    created all methods to set data and create relationships between nodes

--- controllers/data.js
+++ controllers/data.js
@@ -1,42 +1,26 @@
 // movies.js
-const Movies = require('../models/actions/movies');
 const { writeResponse } = require('../helpers/response');
-const loginRequired = require('../middlewares/loginRequired');
 const dbUtils = require('../db/dbUtils');
 const {
   ACTORS: { actors },
-  // todo
-  // DIRECTORS_OF_FILMS: { directors },
+  DIRECTORS_OF_FILMS: { directors },
   GENRES: { genres },
   MOVIES: { movies },
-  // todo
-  // MOVIES_WITH_GENRES: { moviesWithGenres }
+  MOVIES_WITH_GENRES: { moviesWithGenres }
 } = require('../data');
 
 /**
  * @swagger
- * definition:
- *   Movie:
- *     type: object
- *     properties:
- *       id:
- *         type: integer
- *       title:
- *         type: string
- *       summary:
- *         type: object
- *       released:
- *         type: integer
- *       duration:
- *         type: integer
- *       rated:
- *         type: string
- *       tagline:
- *         type: string
- *       poster_image:
- *         type: string
- *       my_rating:
- *         type: integer
+ * /data/genres:
+ *   post:
+ *     tags:
+ *     - data
+ *     description: Set 20 genres
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: "added 20 genres"
  */
 exports.setGenres = function(req, res, next) {
   const session = dbUtils.getSession();
@@ -46,7 +30,19 @@ exports.setGenres = function(req, res, next) {
   });
   writeResponse(res, `added ${genres.length} genres`);
 };
-
+/**
+ * @swagger
+ * /data/movies:
+ *   post:
+ *     tags:
+ *     - data
+ *     description: Set 50 movies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: "added 50 movies"
+ */
 exports.setMovies = function(req, res, next) {
   const session = dbUtils.getSession();
 
@@ -107,432 +103,177 @@ exports.setMovies = function(req, res, next) {
   writeResponse(res, `added ${movies.length} movies`);
   // writeResponse(res, movies[0]);
 };
-// todo
-// eslint-disable-next-line no-unused-vars
-exports.setActors = function(req, res, next) {
-  // const session = dbUtils.getSession();
+/**
+ * @swagger
+ * /data/directors:
+ *   post:
+ *     tags:
+ *     - data
+ *     description: Set directors and their relations to movies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: "added 54 directors with relations [DIRECTED] to their movies"
+ */
+exports.setDirectorsWithRelationshipToMovie = function(req, res, next) {
+  const session = dbUtils.getSession();
 
-  actors.forEach((actor) => {
-    for (const field in actor) {
-      if (actor[field].actor.properties) actor[field].actor = actor[field].actor.properties;
-      if (actor[field].actor.low) actor[field].actor = actor[field].actor.low;
+  directors.forEach((director) => {
+    const { born, died } = director.director.properties;
+
+    director.director.properties.born = [];
+    if (died) director.director.properties.died = [];
+
+    for (const field in born) {
+      if (born) {
+        director.director.properties.born.push(born[field].low);
+      }
+
+      if (died) {
+        director.director.properties.died.push(died[field].low);
+      }
     }
   });
-  console.log(actors);
-  // todo
-  // actors.forEach(({
-  //   actor: {
-  //     bio, born, bornIn, died, imdbId, name, poster, tmdbId, url
-  //   }, movieTitle, role
-  // }) => {
-  //   session.query(
-  //     'MATCH (m:Movie)\n'
-  //     + 'WHERE m.title=\'Toy Story\'\n'
-  //     + 'CREATE (a:Actor)-[r:ACTED_IN_MOVIE {role:\'captain\'}]->(m)\n'
-  //     + 'RETURN type(r), r.name,r.role', { }
-  //   )
-  //     .catch(next);
-  // });
+  directors.forEach(({
+    director: {
+      properties: {
+        bio,
+        born, bornIn, imdbId, name, poster, tmdbId, url
+      }
+    }, movieTitle
+  }) => {
+    session.query(
+      'MATCH (m:Movie) '
+      + 'WHERE m.title=$movieTitle '
+      + 'CREATE (d:Director :Person'
+      + '{'
+      + 'bio:$bio,'
+      + 'born:$born,'
+      + 'bornIn:$bornIn,'
+      + 'imdbId:$imdbId,'
+      + 'name:$name,'
+      + 'poster:$poster,'
+      + 'tmdbId:$tmdbId,'
+      + 'url:$url'
+      + '}'
+      + ')-[r:DIRECTED]->(m)', {
+        bio,
+        born,
+        bornIn,
+        imdbId,
+        name,
+        poster,
+        tmdbId,
+        url,
+        movieTitle
+      }
+    )
+      .catch(next);
+  });
+
+  writeResponse(res, `added ${directors.length} directors with relations [DIRECTED] to their movies`);
+};
+/**
+ * @swagger
+ * /data/actors:
+ *   post:
+ *     tags:
+ *     - data
+ *     description: Set actors and their relations to movies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: "added 192 actors with relations [ACTED_IN_MOVIE] to their movies"
+ */
+exports.setActorsWithRelationshipToMovie = function(req, res, next) {
+  const session = dbUtils.getSession();
+
+  actors.forEach((actor) => {
+    const { born, died } = actor.actor.properties;
+
+    actor.actor.properties.born = [];
+    if (died) actor.actor.properties.died = [];
+
+    for (const field in born) {
+      if (born) {
+        actor.actor.properties.born.push(born[field].low);
+      }
+
+      if (died) {
+        actor.actor.properties.died.push(died[field].low);
+      }
+    }
+  });
+
+  actors.forEach(({
+    actor: {
+      properties: {
+        bio, born, bornIn, died, imdbId, name, poster, tmdbId, url
+      }
+    }, movieTitle, role
+  }) => {
+    session.query(
+      'MATCH (m:Movie) '
+      + 'WHERE m.title=$movieTitle '
+      + 'CREATE (a:Actor :Person'
+      + '{'
+      + 'bio:$bio,'
+      + 'born:$born,'
+      + 'bornIn:$bornIn,'
+      + 'died:$died,'
+      + 'imdbId:$imdbId,'
+      + 'name:$name,'
+      + 'poster:$poster,'
+      + 'tmdbId:$tmdbId,'
+      + 'url:$url'
+      + '}'
+      + ')-[r:ACTED_IN_MOVIE {role: $role}]->(m)', {
+        bio,
+        born,
+        bornIn,
+        died,
+        imdbId,
+        name,
+        poster,
+        tmdbId,
+        url,
+        movieTitle,
+        role
+      }
+    )
+      .catch(next);
+  });
 
   writeResponse(res, `added ${actors.length} actors with relations [ACTED_IN_MOVIE] to their movies`);
 };
-
 /**
  * @swagger
- * /movies:
- *   get:
- *     tags:
- *     - movies
- *     description: Find all movies
- *     summary: Find all movies
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- */
-exports.list = function(req, res, next) {
-  Movies.getAll(dbUtils.getSession())
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/{id}:
- *   get:
- *     tags:
- *     - movies
- *     description: Find movie by ID
- *     summary: Find movie by ID
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: movie id
- *         in: path
- *         required: true
- *         type: integer
- *       - name: Authorization
- *         in: header
- *         type: string
- *         description: Token (token goes here)
- *     responses:
- *       200:
- *         description: A movie
- *         schema:
- *           $ref: '#/definitions/Movie'
- *       404:
- *         description: movie not found
- */
-exports.findById = function(req, res, next) {
-  Movies.getById(dbUtils.getSession(), req.params.id, req.user.id)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/genre/{id}:
- *   get:
- *     tags:
- *     - movies
- *     description: Returns movies based on genre id
- *     summary: Returns movies based on genre id
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: genre id
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       400:
- *         description: Invalid genre id
- */
-exports.findByGenre = function(req, res, next) {
-  const { id } = req.params;
-  if (!id) throw { message: 'Invalid id', status: 400 };
-
-  Movies.getByGenre(dbUtils.getSession(), id)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/daterange/{start}/{end}:
- *   get:
- *     tags:
- *     - movies
- *     description: Returns movies between a year range
- *     summary: Returns movies between a year range
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: start
- *         description: Year that the movie was released on or after
- *         in: path
- *         required: true
- *         type: integer
- *       - name: end
- *         description: Year that the movie was released before
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       400:
- *         description: Error message(s)
- */
-exports.findMoviesByDateRange = function(req, res, next) {
-  const { start } = req.params;
-  const { end } = req.params;
-
-  if (!start) throw { message: 'Invalid start', status: 400 };
-  if (!end) throw { message: 'Invalid end', status: 400 };
-
-  Movies.getByDateRange(dbUtils.getSession(), start, end)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/directed_by/{id}:
- *   get:
- *     tags:
- *     - movies
- *     description: Returns movies directed by a person
- *     summary: Returns movies directed by a person
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: Id of the director person
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       400:
- *         description: Error message(s)
- */
-exports.findMoviesByDirector = function(req, res, next) {
-  const { id } = req.params;
-  if (!id) throw { message: 'Invalid id', status: 400 };
-
-  Movies.getMoviesbyDirector(dbUtils.getSession(), id)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/acted_in_by/{id}:
- *   get:
- *     tags:
- *     - movies
- *     description: Returns movies acted in by some person
- *     summary: Returns movies acted in by some person
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: id of the actor who acted in the movies
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       400:
- *         description: Error message(s)
- */
-exports.findMoviesByActor = function(req, res, next) {
-  const { id } = req.params;
-  if (!id) throw { message: 'Invalid id', status: 400 };
-
-  Movies.getByActor(dbUtils.getSession(req), id)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/written_by/{id}:
- *   get:
- *     tags:
- *     - movies
- *     description: Returns movies written by some person
- *     summary: Returns movies written by some person
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: id of the writer who wrote the movies
- *         in: path
- *         required: true
- *         type: integer
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       400:
- *         description: Error message(s)
- */
-exports.findMoviesByWriter = function(req, res, next) {
-  const { id } = req.params;
-  if (!id) throw { message: 'Invalid id', status: 400 };
-
-  Movies.getMoviesByWriter(dbUtils.getSession(req), id)
-    .then((response) => writeResponse(res, response))
-    .catch(next);
-};
-
-/**
- * @swagger
- * /movies/{id}/rate:
+ * /data/movies_genres:
  *   post:
  *     tags:
- *     - movies
- *     description: Rate a movie from 0-5 inclusive
- *     summary: Rate a movie from 0-5 inclusive
+ *     - data
+ *     description: Set genres  relations to movies
  *     produces:
  *       - application/json
- *     parameters:
- *       - name: id
- *         description: id of the writer who wrote the movies
- *         in: path
- *         required: true
- *         type: integer
- *       - name: body
- *         in: body
- *         type: object
- *         schema:
- *           properties:
- *             rating:
- *               type: integer
- *       - name: Authorization
- *         in: header
- *         type: string
- *         required: true
- *         description: Token (token goes here)
  *     responses:
  *       200:
- *         description: movie rating saved
- *       400:
- *         description: Error message(s)
- *       401:
- *         description: invalid / missing authentication
+ *         description: "added 115 relationships between movies and genres"
  */
-exports.rateMovie = function(req, res, next) {
-  loginRequired(req, res, () => {
-    let { rating } = req.body;
-    rating = Number(rating);
-    if (Number.isNaN(rating) || rating < 0 || rating >= 6) {
-      throw { rating: 'Rating value is invalid', status: 400 };
-    }
+exports.setGenresToMovies = function(req, res, next) {
+  const session = dbUtils.getSession();
 
-    Movies.rate(dbUtils.getSession(), req.params.id, req.user.id, rating)
-      .then(() => writeResponse(res, {}))
+  moviesWithGenres.forEach(({ genre, movieTitle }) => {
+    session.query('MATCH (g:Genre), (m:Movie)\n'
+      + 'WHERE g.name = $genre AND m.title = $movieTitle\n'
+      + 'CREATE (m)-[:IN_GENRE]->(g)\n',
+    {
+      genre, movieTitle
+    })
       .catch(next);
   });
-};
 
-/**
- * @swagger
- * /movies/{id}/rate:
- *   delete:
- *     tags:
- *     - movies
- *     description: Delete your rating for a movie
- *     summary: Delete your rating for a movie
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: id
- *         description: id of the writer who wrote the movies
- *         in: path
- *         required: true
- *         type: integer
- *       - name: Authorization
- *         in: header
- *         type: string
- *         required: true
- *         description: Token (token goes here)
- *     responses:
- *       204:
- *         description: movie rating deleted
- *       400:
- *         description: Error message(s)
- *       401:
- *         description: invalid / missing authentication
- */
-exports.deleteMovieRating = function(req, res, next) {
-  if (!req.params.id) {
-    throw { message: 'Invalid movie id', status: 400 };
-  }
-
-  loginRequired(req, res, () => {
-    Movies.deleteRating(dbUtils.getSession(), req.params.id, req.user.id)
-      .then((response) => writeResponse(res, response, 204))
-      .catch(next);
-  });
-};
-
-/**
- * @swagger
- * /movies/rated:
- *   get:
- *     tags:
- *     - movies
- *     description: A list of movies the authorized user has rated
- *     summary: A list of movies the authorized user has rated
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: Authorization
- *         in: header
- *         type: string
- *         required: true
- *         description: Token (token goes here)
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       401:
- *         description: invalid / missing authentication
- */
-exports.findMoviesRatedByMe = function(req, res, next) {
-  loginRequired(req, res, () => {
-    Movies.getRatedByUser(dbUtils.getSession(), req.user.id)
-      .then((response) => writeResponse(res, response, 200))
-      .catch(next);
-  });
-};
-
-/**
- * @swagger
- * /movies/recommended:
- *   get:
- *     tags:
- *     - movies
- *     description: A list of recommended movies for the authorized user
- *     summary: A list of recommended movies for the authorized user
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: Authorization
- *         in: header
- *         type: string
- *         required: true
- *         description: Token (token goes here)
- *     responses:
- *       200:
- *         description: A list of movies
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Movie'
- *       401:
- *         description: invalid / missing authentication
- */
-exports.getRecommendedMovies = function(req, res, next) {
-  loginRequired(req, res, () => {
-    Movies.getRecommended(dbUtils.getSession(), req.user.id)
-      .then((response) => writeResponse(res, response, 200))
-      .catch(next);
-  });
+  writeResponse(res, `added ${moviesWithGenres.length} relationships between movies and genres`);
+  // writeResponse(res, movies[0]);
 };
--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -3,8 +3,12 @@ const { data } = require('../controllers');
 
 const dataRouter = Router();
 
-dataRouter.post('/genres', data.setGenres);
 dataRouter.post('/movies', data.setMovies);
+dataRouter.post('/genres', data.setGenres);
+dataRouter.post('/movies_genres', data.setGenresToMovies);
+dataRouter.post('/actors', data.setActorsWithRelationshipToMovie);
+dataRouter.post('/directors', data.setDirectorsWithRelationshipToMovie);
+// dataRouter.post('/directors', data.setDirectorsWithRelationshipToMovie);
 // dataRouter.post('/genres',);
 
 module.exports = dataRouter;
